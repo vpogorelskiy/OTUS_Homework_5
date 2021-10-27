46c54e80d383128f1c22c4770af66335b66a0ed3
Vyacheslav Pogorelskiy <vyacheslav.pogorelskiy@fanduel.com>
2021-10-27 19:57:14 +0300

    Created project

---
+++ OTUS_Homework_5/OTUS_Homework_5App.swift
@@ -0,0 +1,17 @@
+//
+//  OTUS_Homework_5App.swift
+//  OTUS_Homework_5
+//
+//  Created by Vyacheslav Pogorelskiy on 27/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct OTUS_Homework_5App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
