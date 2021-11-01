0d8b3260d00c0b41a2c732f96bae104a7db27914
Vyacheslav Pogorelskiy <>
2021-11-01 06:31:16 +0300

    Implemented ViewModel

--- OTUS_Homework_5/UI/ViewModels/DetailViewModel.swift
+++ OTUS_Homework_5/UI/ViewModels/DetailViewModel.swift
@@ -1,12 +1,24 @@
 struct DetailViewModel {
     let text: String
+    let suffixTuples: [(String, Int)]
+    let topTriads: [String]
     
-    var suffixTuples: [(String, Int)] {
-        [("bra", 1), ("cad", 2)]
-    }
-    
-    var topTriads: [String] {
-        ["abr", "cad", "bra", "abc"].sorted()
+    init(text: String) {
+        self.text = text
+        let suffixes = text.suffixArray()
+        let suffixDict = suffixes.reduce(into: [Substring:Int]()) { partialResult, suffix in
+            let count = partialResult[suffix] ?? 0
+            partialResult[suffix] = count + 1
+        }
+        
+        suffixTuples = suffixDict.keys.sorted().reduce(into: [(String, Int)](), { partialResult, suffix in
+            partialResult.append((String(suffix), suffixDict[suffix] ?? 0))
+        })
+        
+        topTriads = suffixDict.filter{ $0.key.count == 3 }
+                            .sorted(by: { $1.key > $0.key })
+                            .prefix(10)
+                            .map{ String($0.key) }
     }
 }
 
